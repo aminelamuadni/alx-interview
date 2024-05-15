981fd2d2e2cd611c41c3473a6f4bf711d0c03f9b
Amine Lamuadni <W7equwE5@gmail.com>
2024-05-15 22:28:22 +0100

    Update 0-nqueens.py file

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,28 +1,54 @@
 #!/usr/bin/python3
 import sys
 
+"""
+This module solves the N Queens problem using a backtracking algorithm. It
+takes a single command-line argument, the size of the board N, where N must be
+an integer greater than or equal to 4, and prints all possible arrangements of
+N queens on an N x N chessboard where no two queens threaten each other.
+"""
+
 
 def print_usage_and_exit():
+    """
+    Prints the usage of the script and exits. This function is called when
+    there is an error with command-line arguments.
+    """
     print("Usage: nqueens N")
     sys.exit(1)
 
 
 def is_valid_position(board, row, col):
+    """
+    Checks if it's safe to place a queen at board[row][col]. This function
+    verifies that no other queens are placed in the same column or diagonal.
+
+    :param board: list, current positions of queens on the board
+    :param row: int, row index where the queen is being placed
+    :param col: int, column index where the queen is being placed
+    :return: bool, True if safe, False otherwise
+    """
     for i in range(row):
-        # Check column and diagonal conflicts
-        if board[i] == col or \
-           board[i] - i == col - row or \
-           board[i] + i == col + row:
+        if (board[i] == col or
+           board[i] - i == col - row or
+           board[i] + i == col + row):
             return False
     return True
 
 
 def solve_nqueens(n, row, board, solutions):
+    """
+    Recursively attempts to place queens on the board and records all solutions
+    where queens do not threaten each other.
+
+    :param n: int, the size of the chessboard and number of queens
+    :param row: int, current row to try placing a queen
+    :param board: list, current positions of queens on the board
+    :param solutions: list, list to hold all of the board configurations that
+    solve the problem
+    """
     if row == n:
-        solution = []
-        for i in range(n):
-            solution.append([i, board[i]])
-        solutions.append(solution)
+        solutions.append([[i, board[i]] for i in range(n)])
         return
 
     for col in range(n):
@@ -33,6 +59,10 @@ def solve_nqueens(n, row, board, solutions):
 
 
 def main():
+    """
+    Main function to handle command-line inputs and trigger the backtracking
+    solution process.
+    """
     if len(sys.argv) != 2:
         print_usage_and_exit()
 
