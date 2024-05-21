ca36de578b6b78338581a6a50dc4450247e511d4
Amine Lamuadni <W7equwE5@gmail.com>
2024-05-22 00:43:28 +0100

    Update 0-starwars_characters.js file

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,7 +5,7 @@ const movieId = process.argv[2];
 request(`https://swapi-api.alx-tools.com/api/films/${movieId}/`, { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
   let characters = body.characters;
-  characters.forEach(charUrl => {
+  characters.forEach((charUrl) => {
     request(charUrl, { json: true }, (err, res, body) => {
       if (err) { return console.log(err); }
       console.log(body.name);
