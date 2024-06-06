39a1696225e487ff48ebcfb514eeda68663413fb
Amine Lamuadni <W7equwE5@gmail.com>
2024-06-06 18:34:17 +0100

    Update 0-making_change.py file

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """
-This module contains a function makeChange which solves the coin change problem
-using dynamic programming.
-It aims to find the minimum number of coins needed to make a given total from a
-list of coin denominations.
+This module determines the minimum number of coins needed for a given total
+using various denominations.
 """
 
 
@@ -13,23 +11,27 @@ def makeChange(coins, total):
     total.
 
     Args:
-        coins (list): A list of coin denominations available.
-        total (int): The total amount for which change needs to be made.
+        coins (list): List of coin denominations available.
+        total (int): The total amount to make change for.
 
     Returns:
         int: The minimum number of coins needed to make change for the given
              total.
-             Returns -1 if it is not possible to make change for the given
-             total.
+             Returns -1 if it is not possible to make change for the total
+             using the given coins.
     """
     if total <= 0:
         return 0
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    if not coins:
+        return -1
+
+    coinCounts = [float('inf')] * (total + 1)
+    coinCounts[0] = 0
 
     for coin in coins:
-        for j in range(coin, total + 1):
-            if dp[j - coin] != float('inf'):
-                dp[j] = min(dp[j], dp[j - coin] + 1)
+        for amount in range(coin, total + 1):
+            if coinCounts[amount - coin] != float('inf'):
+                coinCounts[amount] = min(coinCounts[amount],
+                                         coinCounts[amount - coin] + 1)
 
-    return -1 if dp[total] == float('inf') else dp[total]
+    return coinCounts[total] if coinCounts[total] != float('inf') else -1
